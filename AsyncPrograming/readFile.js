26af0953f9cc6da869fbd132186d24bb19cf0678
Ádám Szabó <szabo.adam.tamas@gmail.com>
2015-09-23 11:01:22 +0200

    Async read #1

--- AsyncPrograming/readFile.js
+++ AsyncPrograming/readFile.js
@@ -1,1 +1,11 @@
-//Implement me plz :(
+var fs = require('fs');
+
+fs.readFile(__filename, {encoding: 'utf8'}, gotFileContents);
+
+function gotFileContents(err, result) {
+  if(err) {
+    return console.error(err);
+  }
+
+  console.log(result);
+}
